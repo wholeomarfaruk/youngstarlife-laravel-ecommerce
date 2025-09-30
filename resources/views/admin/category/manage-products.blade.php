736af728ab534e98603826ffd7d1b4904ff13bf0
wholeomarfaruk <wholeomarfaruk@gmail.com>
2025-09-30 15:11:01 +0600

    update

--- resources/views/admin/category/manage-products.blade.php
+++ resources/views/admin/category/manage-products.blade.php
@@ -186,7 +186,7 @@
             </div>
             <div class="divider"></div>
             <div class="flex items-center justify-between flex-wrap gap10 wgp-pagination mt-5">
-                {{ $Categoryproducts->links('pagination::bootstrap-5') }}
+                {{-- {{ $Categoryproducts->links('pagination::bootstrap-5') }} --}}
             </div>
         </div>
     </div>
