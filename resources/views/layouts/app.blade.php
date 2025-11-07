00dfeb4de662835fb807cd581e3b06192b64d228
wholeomarfaruk <wholeomarfaruk@gmail.com>
2025-11-07 23:24:34 +0600

    coment tawk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -264,7 +264,7 @@
 
     @stack('scripts')
     <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
+    {{-- <script type="text/javascript">
         var Tawk_API = Tawk_API || {},
             Tawk_LoadStart = new Date();
         (function() {
@@ -276,7 +276,7 @@
             s1.setAttribute('crossorigin', '*');
             s0.parentNode.insertBefore(s1, s0);
         })();
-    </script>
+    </script> --}}
     <!--End of Tawk.to Script-->
 </body>
 
